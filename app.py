4ba8e35cc8e43d6cdfbc263d06510d92a4adafe0
Gyanendra Jha <mailgjhaa@gmail.com>
2026-05-19 17:28:26 +0530

    Added WhatsApp route

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "OK"
+
+    return "AI WhatsApp Student Bot Running"
 
 
 @app.route("/health")
 def health():
+
     return "healthy"
+
+
+@app.route("/whatsapp", methods=["GET", "POST"])
+def whatsapp():
+
+    return "WhatsApp webhook working"
